78ef13220feaf2c1caf0ee7d61abedb11464242c
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-04-12 18:37:59 -0500

    Adds New Methods To BasicsDemo Class.

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -95,12 +95,27 @@ class BasicsDemo {
         System.out.println("Student 3: " + students[2].name);
     }
 
+    static double sum(double x, double y) {
+        return x + y;
+    }
+
+    static double avg(double x, double y) {
+        double sum = sum(x, y);
+        return sum / 2;
+    }
+
     public static void main(String[] args) {
 
         // Language Basics 1
         // print();
         // primitives();
         // typeCasting();
-        arrays();
+        // arrays();
+
+        double d = sum(3.0f, (float) 2.0);
+        System.out.println(d);
+
+        double d2 = avg(3.0, 2.0);
+        System.out.println(d2);
     }
 }
