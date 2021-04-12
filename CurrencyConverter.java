8a7d4dbd964076fd9b9c0253f8aaeb50c3331f3b
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-04-12 17:44:19 -0500

    Solution To Arrays Assignment.

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,23 +1,13 @@
 public class CurrencyConverter {
-    int rupee = 63;
-    int dirham = 3;
-    int real = 3;
-    int chilean_peso = 595;
-    int mexican_peso = 18;
-    int _yen = 107;
-    int $australian = 2;
-    int dollar;
-    int Rupee = 63;
+    double[] exchangeRates = { 63, 3, 3, 595, 18, 107, 2 };
 
     void printCurrencies() {
-        System.out.println("rupee: " + rupee);
-        System.out.println("dirham: " + dirham);
-        System.out.println("real: " + real);
-        System.out.println("chilean_peso: " + chilean_peso);
-        System.out.println("mexican_peso: " + mexican_peso);
-        System.out.println("$australian: " + $australian);
-        System.out.println("dollar: " + dollar);
-        System.out.println("Rupee: " + Rupee);
+        System.out.println("rupee: " + exchangeRates[0]);
+        System.out.println("dirham: " + exchangeRates[1]);
+        System.out.println("real: " + exchangeRates[2]);
+        System.out.println("chilean_peso: " + exchangeRates[3]);
+        System.out.println("mexican_peso: " + exchangeRates[4]);
+        System.out.println("$australian: " + exchangeRates[5]);
     }
 
     public static void main(String[] args) {
